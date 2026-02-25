02fa1b3bdd83f4fedc8c773d09c93488b2cb7573
Bruno Henrique Sena <141459886+Bruno-Sena-Dev@users.noreply.github.com>
2026-02-24 21:12:29 -0400

    Wrap App component in React.StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/my-certificate-vault">
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter basename="/my-certificate-vault">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
